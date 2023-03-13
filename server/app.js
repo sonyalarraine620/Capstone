33b0d78f037ea262b1997211461598a3f242ad81
Sonya Villarruel <sonyalarraine620@outlook.com>
2023-03-13 15:30:24 -0700

    Mongoose

--- server/app.js
+++ server/app.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
+
+
+const PORT = process.env.PORT || 4040;
+mongoose.connect(process.env.MONGODB);
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
+
 // Request handlers go here
 const logging = (request, response, next) => {
     console.log(`${request.method} ${request.url} ${Date.now()}`);
